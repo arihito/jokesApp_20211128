c93f4d47e50ef343cda573f89c2aa85d5e683320
ArihitoMatsuda <arihito.m@gmail.com>
2021-11-28 15:03:01 +0900

    add: Routes Parameterized Routes

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,10 @@
+export default function JokeRoute() {
+  return (
+    <div>
+      <p>これがあなたの陽気なジョークです：</p>
+      <p>
+        木に隠れているカバを見つけてみませんか？ 彼らは本当にそれが得意です。
+      </p>
+    </div>
+  );
+}
---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,10 @@
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>これがランダムなジョークです:</p>
+      <p>
+       なぜフリスビーが大きくなっているのだろうと思ったら、それが私にぶつかった。
+      </p>
+    </div>
+  );
+}
